570b0d673918bef3c2d844dc2ae188a1729cd503
Ronit Chhabra <rchhabr2@binghamton.edu>
2021-03-07 17:39:30 -0500

    Draft changes

--- src/draft.py
+++ src/draft.py
@@ -7,6 +7,11 @@ class Draft:
         self.round_counter = 1
         with open('../data/players.pkl', 'rb') as inp:
             self.players = pickle.load(inp)
+        for i in self.players:
+            print(i.attributes)
+        self.players = self.sortByOvr(self.players)
+        #for i in self.players:
+            #print(i.attributes)
 
     """
     Return list of players you will select from depending on the round.
@@ -17,9 +22,10 @@ class Draft:
 
         self.round_counter += 1
         return []
-
+    
+    def sortByOvr(self, ls):
+        sorted(ls, key=lambda x: x.attributes.get("Overall"))
+        return ls
 if __name__ == '__main__':
-    with open('../data/players.pkl', 'rb') as inp:
-        players = pickle.load(inp)
-    y = players[0].getOverall()
-    print(y)
+    d = Draft()
+    
