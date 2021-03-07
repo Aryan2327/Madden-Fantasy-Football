2be9cae7e6a7adc60ad2bbe82c33b61812282bca
Aryan2327 <achaudh9@binghamton.edu>
2021-03-07 13:00:55 -0500

    class

---
+++ src/draft.py
@@ -0,0 +1,4 @@
+class Draft:
+    def __init__(self):
+        self.rounds = 13
+        self.teams = 10
--- src/positions.py
+++ src/positions.py
@@ -11,6 +11,11 @@ class WR(Player):
         super.__init__(attributes)
 
 
+class HB(Player):
+    def __init__(self, attributes):
+        super.__init__(attributes)
+
+
 class TE(Player):
     def __init__(self, attributes):
         super.__init__(attributes)
