0189b96aa8af17bc8109e2df0b8f5dcab7ab544d
Aryan2327 <achaudh9@binghamton.edu>
2021-03-07 11:58:28 -0500

    some classes added

---
+++ src/player.py
@@ -0,0 +1,6 @@
+class Player:
+    """
+    Param(attributes): Takes a dictionary of all of the player stats
+    """
+    def __init__(self, attributes):
+        pass
---
+++ src/positions.py
@@ -0,0 +1,17 @@
+from player import Player
+
+
+class QB(Player):
+    pass
+
+
+class WR(Player):
+    pass
+
+
+class TE(Player):
+    pass
+
+
+class K(Player):
+    pass
